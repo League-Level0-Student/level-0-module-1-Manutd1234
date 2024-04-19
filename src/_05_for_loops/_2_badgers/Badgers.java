761f003d67fbd167104fe582c90da3d1e9637d47
league <hailinday@gmail.com>
2024-04-18 19:54:36 -0700

    Done with Badgers

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -2,9 +2,30 @@ package _05_for_loops._2_badgers;
 
 public class Badgers {
 	public static void main(String[] args) {
-		for(int badgers = 0; badgers <12; badgers +=1) {
-			System.out.print("Badger, ");
+		for (int badgers1 = 0; badgers1 < 2; badgers1 += 1) {
+			for (int badgers = 0; badgers < 12; badgers += 1) {
 
+				System.out.print("Badger");
+				if (badgers < 11) {
+					System.out.print(", ");
+				}
+			}
+
+		
+		
+
+			System.out.println();
+			for (int mushroom = 0; mushroom < 2; mushroom += 1) {
+				System.out.print("Mushroom");
+				if (mushroom < 1) {
+					System.out.print(", ");
+				}
+			}
+			System.out.println();
+			System.out.println();
 		}
+		
+			System.out.print("A Snake!!!");
+		}
+		
 	}
-}
